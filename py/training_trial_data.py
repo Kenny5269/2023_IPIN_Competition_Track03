3c97bbef259c43a11dc2f4edeb0def8a736010c0
kenny <kennyapple14@gmail.com>
2025-04-22 06:46:59 +0800

    v2.2 訓練保留內插GT，測試只用真實GT

--- py/training_trial_data.py
+++ py/training_trial_data.py
@@ -8,13 +8,16 @@ import matplotlib.pyplot as plt
 def plot_figure(aligned_data_train, aligned_data_test):
     # 可視化：軌跡圖
     plt.figure(figsize=(8, 6))
-    gt_coords_origin = [(d["gt_lat"], d["gt_lon"]) for d in aligned_data_train]
-    gt_coords = [(d["gt_lat"], d["gt_lon"]) for d in aligned_data_test]
-    init_coords = [(d["init_lat"], d["init_lon"]) for d in aligned_data_test]
-    pdr_coords = [(d["pdr_lat"], d["pdr_lon"]) for d in aligned_data_test]
+    gt_coords_origin = [(d["gt_lat"], d["gt_lon"]) for d in aligned_data_train if d["gt_lat"] is not None]
+    gt_coords = [(d["gt_lat"], d["gt_lon"]) for d in aligned_data_test if d["gt_lat"] is not None]
+    print(len(gt_coords))
+    init_coords = [(d["init_lat"], d["init_lon"]) for d in aligned_data_test if d["gt_lat"] is not None]
+    print(len(init_coords))
+    #pdr_coords = [(d["pdr_lat"], d["pdr_lon"]) for d in aligned_data_test]
     #pdr_tra = [(d["pdr_trajectory"]) for d in aligned_data]
-    pdr_tra = [pt for d in aligned_data_test for pt in d["pdr_trajectory"]]
-    fused_coords = [(d["fused_lat"], d["fused_lon"]) for d in aligned_data_test]
+    #pdr_tra = [pt for d in aligned_data_test for pt in d["pdr_trajectory"]]
+    fused_coords = [(d["fused_lat"], d["fused_lon"]) for d in aligned_data_test if d["gt_lat"] is not None]
+    print(len(fused_coords))
     # print(len(gt_coords))
     # print(len(pdr_coords))
 
@@ -22,17 +25,17 @@ def plot_figure(aligned_data_train, aligned_data_test):
     gt_lats_ori, gt_lons_ori = zip(*gt_coords_origin)
     gt_lats, gt_lons = zip(*gt_coords)
     init_lats, init_lons = zip(*init_coords)
-    pdr_lats, pdr_lons = zip(*pdr_coords)
-    tra_lats, tra_lons = zip(*pdr_tra)
+    #pdr_lats, pdr_lons = zip(*pdr_coords)
+    #tra_lats, tra_lons = zip(*pdr_tra)
     fused_lat, fused_lon = zip(*fused_coords)
 
 
     #plt.plot(gt_lons_ori, gt_lats_ori, label="Ground Truth origin", marker="o")
     plt.plot(gt_lons, gt_lats, label="Ground Truth", marker="o")
-    plt.plot(init_lons, init_lats, label="Wi-Fi Init", marker="x")
+    #plt.plot(init_lons, init_lats, label="Wi-Fi Init", marker="x")
     #plt.plot(pdr_lons, pdr_lats, label="IMU PDR", marker="^")
     #plt.plot(tra_lons, tra_lats, label="IMU PDR", marker="^")
-    #plt.plot(fused_lon, fused_lat, label="Wifi PDR Fused", marker="^")
+    plt.plot(fused_lon, fused_lat, label="Wifi PDR Fused", marker="^")
 
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
@@ -46,31 +49,31 @@ def all_errors(aligned_data):
     num = 0
     wifi_init_errors = [
         geodesic((d["gt_lat"], d["gt_lon"]), (d["init_lat"], d["init_lon"])).meters
-        for d in aligned_data
+        for d in aligned_data if d["gt_lat"] is not None
     ]
     wifi_init_rmse = np.sqrt(np.mean(np.square(wifi_init_errors)))
     wifi_init_mean_error = np.mean(wifi_init_errors)
     wifi_init_std_error = np.std(wifi_init_errors)
     wifi_init_max_error = np.max(wifi_init_errors)
 
-    pdr_errors = []
-    for d in aligned_data:
-        if num == 0:
-            prev_pdr_lat = d["pdr_lat"]
-            prev_pdr_lon = d["pdr_lon"]
-            num += 1
-            continue
-        pdr_errors.append(geodesic((d["gt_lat"], d["gt_lon"]), (prev_pdr_lat, prev_pdr_lon)).meters)
-        prev_pdr_lat = d["pdr_lat"]
-        prev_pdr_lon = d["pdr_lon"]
-    pdr_rmse = np.sqrt(np.mean(np.square(pdr_errors)))
-    pdr_mean_error = np.mean(pdr_errors)
-    pdr_std_error = np.std(pdr_errors)
-    pdr_max_error = np.max(pdr_errors)
+    # pdr_errors = []
+    # for d in aligned_data:
+    #     if num == 0:
+    #         prev_pdr_lat = d["pdr_lat"]
+    #         prev_pdr_lon = d["pdr_lon"]
+    #         num += 1
+    #         continue
+    #     pdr_errors.append(geodesic((d["gt_lat"], d["gt_lon"]), (prev_pdr_lat, prev_pdr_lon)).meters)
+    #     prev_pdr_lat = d["pdr_lat"]
+    #     prev_pdr_lon = d["pdr_lon"]
+    # pdr_rmse = np.sqrt(np.mean(np.square(pdr_errors)))
+    # pdr_mean_error = np.mean(pdr_errors)
+    # pdr_std_error = np.std(pdr_errors)
+    # pdr_max_error = np.max(pdr_errors)
 
     fused_errors = [
         geodesic((d["gt_lat"], d["gt_lon"]), (d["fused_lat"], d["fused_lon"])).meters
-        for d in aligned_data
+        for d in aligned_data if d["gt_lat"] is not None
     ]
     fused_rmse = np.sqrt(np.mean(np.square(fused_errors)))
     fused_mean_error = np.mean(fused_errors)
@@ -78,23 +81,13 @@ def all_errors(aligned_data):
     fused_max_error = np.max(fused_errors)
 
     print(f'wifi_init_rmse = {wifi_init_rmse:.2f} m, wifi_init_mean_error = {wifi_init_mean_error:.2f} m, wifi_init_std_error = {wifi_init_std_error:.2f} m, wifi_init_max_error = {wifi_init_max_error:.2f} m')
-    print(f'pdr_rmse = {pdr_rmse:.2f} m, pdr_mean_error = {pdr_mean_error:.2f} m, pdr_std_error = {pdr_std_error:.2f} m, pdr_max_error = {pdr_max_error:.2f} m')
+    #print(f'pdr_rmse = {pdr_rmse:.2f} m, pdr_mean_error = {pdr_mean_error:.2f} m, pdr_std_error = {pdr_std_error:.2f} m, pdr_max_error = {pdr_max_error:.2f} m')
     print(f'fused_rmse = {fused_rmse:.2f} m, fused_mean_error = {fused_mean_error:.2f} m, fused_std_error = {fused_std_error:.2f} m, fused_max_error = {fused_max_error:.2f} m')
 
 def evaluate_errors(aligned_data, pred_key_lat="fused_lat", pred_key_lon="fused_lon", plot_cdf=True):
-    gt_coords = []
-    pred_coords = []
-    for d in aligned_data:
-        if d["gt_lat"] is not None and d["gt_lon"] is not None:
-            gt_coords.append([d["gt_lat"], d["gt_lon"]])
-            pred_coords.append([d[pred_key_lat], d[pred_key_lon]])
-
-    gt_coords = np.array(gt_coords)
-    pred_coords = np.array(pred_coords)
-
     dists = np.array([
-        geodesic(gt, pred).meters
-        for gt, pred in zip(gt_coords, pred_coords)
+        geodesic((d["gt_lat"], d["gt_lon"]), (d[pred_key_lat], d[pred_key_lon])).meters
+        for d in aligned_data if d["gt_lat"] is not None
     ])
     rmse = np.sqrt(np.mean(dists ** 2))
     mean_error = np.mean(dists)
@@ -113,10 +106,10 @@ def evaluate_errors(aligned_data, pred_key_lat="fused_lat", pred_key_lon="fused_
         plt.tight_layout()
         plt.show()
 
-    print(f"RMSE: {rmse:.2f} m")
-    print(f"Mean Error: {mean_error:.2f} m")
-    print(f"Std Deviation: {std_error:.2f} m")
-    print(f"Max Error: {max_error:.2f} m")
+    # print(f"RMSE: {rmse:.2f} m")
+    # print(f"Mean Error: {mean_error:.2f} m")
+    # print(f"Std Deviation: {std_error:.2f} m")
+    # print(f"Max Error: {max_error:.2f} m")
     return {
         "rmse": rmse,
         "mean": mean_error,
@@ -157,13 +150,13 @@ if __name__ == '__main__':
     aligned_data_test3 = trial_dict['test_trial03']
     aligned_data_test4 = trial_dict['test_trial04']
 
-    evaluate_errors(aligned_data_train)
-    all_errors(aligned_data_train)
-    plot_figure(aligned_data_train, aligned_data_train)
+    # evaluate_errors(aligned_data_train)
+    # all_errors(aligned_data_train)
+    # plot_figure(aligned_data_train, aligned_data_train)
 
-    # evaluate_errors(aligned_data_test1)
-    # all_errors(aligned_data_test1)
-    # plot_figure(aligned_data_train, aligned_data_test1)
+    #evaluate_errors(aligned_data_test1)
+    all_errors(aligned_data_test1)
+    plot_figure(aligned_data_train, aligned_data_test1)
 
     # evaluate_errors(aligned_data_test2)
     # all_errors(aligned_data_test2)
--- py/wifi_imu_preprocessing_multi.py
+++ py/wifi_imu_preprocessing_multi.py
@@ -40,13 +40,13 @@ for i in index2:
     TEMP_IMU_CSV.append(f"py/T{i}_R1/IMU_50Hz.csv")
     TEMP_GT_CSV.append(f"py/T{i}_R1/POSI2.csv")
 
-TEST_WIFI_CSV = "py/TEST1/WIFI_merged2.csv"
-TEST_IMU_CSV = "py/TEST1/IMU_50Hz.csv"
-TEST_GT_CSV = "py/TEST1/POSI2.csv"
+TEST_WIFI_CSV = "py/TEST4/WIFI_merged2.csv"
+TEST_IMU_CSV = "py/TEST4/IMU_50Hz.csv"
+TEST_GT_CSV = "py/TEST4/POSI2.csv"
 
 OUTPUT_DIR = f"py/aligned_trials/all_trial"
 TEMP_OUTPUT_DIR = f"py/aligned_trials/temp_trial"
-TEST_OUTPUT_DIR = f"py/aligned_trials/test_trial01"
+TEST_OUTPUT_DIR = f"py/aligned_trials/test_trial04"
 
 IMU_WINDOW_SEC = 4.0
 STEP_THRESHOLD = 1.2
@@ -391,14 +391,13 @@ for x in range(len(TEMP_WIFI_CSV)):
     # 根據 GT 資料補上 aligned_data 對應時間點的 gt_lat/lon
     used_indices = set()
     posi_aligned_indices = []
-    for i in range(len(aligned_data)):
-        if aligned_data[i]["timestamp"] < posi_df.loc[0, "timestamp"]:
-            aligned_data[i]["gt_lat"] = posi_df.loc[0, "Latitude_degrees"]
-            aligned_data[i]["gt_lon"] = posi_df.loc[0, "Longitude_degrees"]
-            #used_indices.add(i)
-        elif aligned_data[i]["timestamp"] > posi_df.loc[len(posi_df)-1, "timestamp"]:
-            aligned_data[i]["gt_lat"] = posi_df.loc[len(posi_df)-1, "Latitude_degrees"]
-            aligned_data[i]["gt_lon"] = posi_df.loc[len(posi_df)-1, "Longitude_degrees"]
+    # for i in range(len(aligned_data)):
+    #     if aligned_data[i]["timestamp"] < posi_df.loc[0, "timestamp"]:
+    #         aligned_data[i]["gt_lat"] = posi_df.loc[0, "Latitude_degrees"]
+    #         aligned_data[i]["gt_lon"] = posi_df.loc[0, "Longitude_degrees"]
+    #     elif aligned_data[i]["timestamp"] > posi_df.loc[len(posi_df)-1, "timestamp"]:
+    #         aligned_data[i]["gt_lat"] = posi_df.loc[len(posi_df)-1, "Latitude_degrees"]
+    #         aligned_data[i]["gt_lon"] = posi_df.loc[len(posi_df)-1, "Longitude_degrees"]
     for _, gt_row in posi_df.iterrows():
         gt_time = gt_row["timestamp"]
         closest_idx = min(
@@ -413,18 +412,18 @@ for x in range(len(TEMP_WIFI_CSV)):
             posi_aligned_indices.append(closest_idx)
 
     # 依據已知 GT 點之間做線性插值填補其餘點
-    for i in range(1, len(posi_aligned_indices)):
-        start_idx = posi_aligned_indices[i - 1]
-        end_idx = posi_aligned_indices[i]
-        start_lat, start_lon = aligned_data[start_idx]["gt_lat"], aligned_data[start_idx]["gt_lon"]
-        end_lat, end_lon = aligned_data[end_idx]["gt_lat"], aligned_data[end_idx]["gt_lon"]
-        steps = end_idx - start_idx
-        for j in range(1, steps):
-            ratio = j / steps
-            interp_lat = start_lat + ratio * (end_lat - start_lat)
-            interp_lon = start_lon + ratio * (end_lon - start_lon)
-            aligned_data[start_idx + j]["gt_lat"] = interp_lat
-            aligned_data[start_idx + j]["gt_lon"] = interp_lon
+    # for i in range(1, len(posi_aligned_indices)):
+    #     start_idx = posi_aligned_indices[i - 1]
+    #     end_idx = posi_aligned_indices[i]
+    #     start_lat, start_lon = aligned_data[start_idx]["gt_lat"], aligned_data[start_idx]["gt_lon"]
+    #     end_lat, end_lon = aligned_data[end_idx]["gt_lat"], aligned_data[end_idx]["gt_lon"]
+    #     steps = end_idx - start_idx
+    #     for j in range(1, steps):
+    #         ratio = j / steps
+    #         interp_lat = start_lat + ratio * (end_lat - start_lat)
+    #         interp_lon = start_lon + ratio * (end_lon - start_lon)
+    #         aligned_data[start_idx + j]["gt_lat"] = interp_lat
+    #         aligned_data[start_idx + j]["gt_lon"] = interp_lon
 
     for i in aligned_data:
         aligned_data_temp.append(i)
@@ -451,6 +450,13 @@ for i in range(len(wifi_df)):
     ].drop(columns=["SensorTimestamp(s)"]).reset_index(drop=True)
 
     test_aligned_data.append({
+        "init_lat" : None,
+        "init_lon" : None,
+        "pdr_trajectory" : None,
+        "pdr_lat" : None,
+        "pdr_lon" : None,
+        "fused_lat" : None,
+        "fused_lon" : None,
         "timestamp": wifi_time,
         "rssi_vector": rssi_vector,
         "imu_window": imu_window,
@@ -464,14 +470,13 @@ for i in range(len(wifi_df)):
 used_indices = set()
 posi_aligned_indices = []
 
-for i in range(len(test_aligned_data)):
-    if test_aligned_data[i]["timestamp"] < posi_df.loc[0, "timestamp"]:
-        test_aligned_data[i]["gt_lat"] = posi_df.loc[0, "Latitude_degrees"]
-        test_aligned_data[i]["gt_lon"] = posi_df.loc[0, "Longitude_degrees"]
-        #used_indices.add(i)
-    elif test_aligned_data[i]["timestamp"] > posi_df.loc[len(posi_df)-1, "timestamp"]:
-        test_aligned_data[i]["gt_lat"] = posi_df.loc[len(posi_df)-1, "Latitude_degrees"]
-        test_aligned_data[i]["gt_lon"] = posi_df.loc[len(posi_df)-1, "Longitude_degrees"]
+# for i in range(len(test_aligned_data)):
+#     if test_aligned_data[i]["timestamp"] < posi_df.loc[0, "timestamp"]:
+#         test_aligned_data[i]["gt_lat"] = posi_df.loc[0, "Latitude_degrees"]
+#         test_aligned_data[i]["gt_lon"] = posi_df.loc[0, "Longitude_degrees"]
+#     elif test_aligned_data[i]["timestamp"] > posi_df.loc[len(posi_df)-1, "timestamp"]:
+#         test_aligned_data[i]["gt_lat"] = posi_df.loc[len(posi_df)-1, "Latitude_degrees"]
+#         test_aligned_data[i]["gt_lon"] = posi_df.loc[len(posi_df)-1, "Longitude_degrees"]
 for _, gt_row in posi_df.iterrows():
     gt_time = gt_row["timestamp"]
     closest_idx = min(
@@ -486,18 +491,18 @@ for _, gt_row in posi_df.iterrows():
         posi_aligned_indices.append(closest_idx)
 
 # 依據已知 GT 點之間做線性插值填補其餘點
-for i in range(1, len(posi_aligned_indices)):
-    start_idx = posi_aligned_indices[i - 1]
-    end_idx = posi_aligned_indices[i]
-    start_lat, start_lon = test_aligned_data[start_idx]["gt_lat"], test_aligned_data[start_idx]["gt_lon"]
-    end_lat, end_lon = test_aligned_data[end_idx]["gt_lat"], test_aligned_data[end_idx]["gt_lon"]
-    steps = end_idx - start_idx
-    for j in range(1, steps):
-        ratio = j / steps
-        interp_lat = start_lat + ratio * (end_lat - start_lat)
-        interp_lon = start_lon + ratio * (end_lon - start_lon)
-        test_aligned_data[start_idx + j]["gt_lat"] = interp_lat
-        test_aligned_data[start_idx + j]["gt_lon"] = interp_lon
+# for i in range(1, len(posi_aligned_indices)):
+#     start_idx = posi_aligned_indices[i - 1]
+#     end_idx = posi_aligned_indices[i]
+#     start_lat, start_lon = test_aligned_data[start_idx]["gt_lat"], test_aligned_data[start_idx]["gt_lon"]
+#     end_lat, end_lon = test_aligned_data[end_idx]["gt_lat"], test_aligned_data[end_idx]["gt_lon"]
+#     steps = end_idx - start_idx
+#     for j in range(1, steps):
+#         ratio = j / steps
+#         interp_lat = start_lat + ratio * (end_lat - start_lat)
+#         interp_lon = start_lon + ratio * (end_lon - start_lon)
+#         test_aligned_data[start_idx + j]["gt_lat"] = interp_lat
+#         test_aligned_data[start_idx + j]["gt_lon"] = interp_lon
 #-----------------------------------------------------------------------------------------------------------------------------
 # WIFI RSSI 初始定位
 rssi_features = []
@@ -571,6 +576,7 @@ for i, d in enumerate(aligned_data_all):
     d["pdr_lat"], d["pdr_lon"] = pdr_trajectory[-1]
 
 # WIFI RSSI 初始定位(臨時測試軌跡)
+first = False
 for i, d in enumerate(aligned_data_temp):
     rssi = np.nan_to_num(d["rssi_vector"], nan=-100.0)
     init_lat, init_lon = predict_wifi_position(wifi_model, rssi)
@@ -579,12 +585,17 @@ for i, d in enumerate(aligned_data_temp):
     d["init_lat"] = init_lat
     d["init_lon"] = init_lon
 
-    if i == 0:
+    if not first and d["gt_lat"] is not None:
+        first = True
+        print('first')
         d["fused_lat"] = init_lat
         d["fused_lon"] = init_lon
         pdr_trajectory = estimate_trajectory_from_imu_all(d["fused_lat"], d["fused_lon"], imu_seq)
         d["pdr_trajectory"] = pdr_trajectory
         d["pdr_lat"], d["pdr_lon"] = pdr_trajectory[-1]
+        continue
+    elif not first:
+        print('waiting')
         continue
 
     # 融合 init 與上一步 pdr 座標
@@ -628,6 +639,7 @@ for i, d in enumerate(aligned_data_temp):
     d["pdr_lat"], d["pdr_lon"] = pdr_trajectory[-1]
 
 # WIFI RSSI 初始定位(測試軌跡)
+first = False
 for i, d in enumerate(test_aligned_data):
     rssi = np.nan_to_num(d["rssi_vector"], nan=-100.0)
     init_lat, init_lon = predict_wifi_position(wifi_model, rssi)
@@ -636,12 +648,17 @@ for i, d in enumerate(test_aligned_data):
     d["init_lat"] = init_lat
     d["init_lon"] = init_lon
 
-    if i == 0:
+    if not first and d["gt_lat"] is not None:
+        first = True
+        print('first')
         d["fused_lat"] = init_lat
         d["fused_lon"] = init_lon
         pdr_trajectory = estimate_trajectory_from_imu_all(d["fused_lat"], d["fused_lon"], imu_seq)
         d["pdr_trajectory"] = pdr_trajectory
         d["pdr_lat"], d["pdr_lon"] = pdr_trajectory[-1]
+        continue
+    elif not first:
+        print('waiting')
         continue
 
     # 融合 init 與上一步 pdr 座標
@@ -685,14 +702,14 @@ for i, d in enumerate(test_aligned_data):
     d["pdr_lat"], d["pdr_lon"] = pdr_trajectory[-1]
 
 # 輸出所有對齊資料為 pickle
-for i, d in enumerate(aligned_data_all):
-    with open(os.path.join(OUTPUT_DIR, f"sample_{i:04d}.pkl"), "wb") as f:
-        pickle.dump(d, f)
+# for i, d in enumerate(aligned_data_all):
+#     with open(os.path.join(OUTPUT_DIR, f"sample_{i:04d}.pkl"), "wb") as f:
+#         pickle.dump(d, f)
 
 # 輸出所有對齊資料為 pickle
-for i, d in enumerate(aligned_data_temp):
-    with open(os.path.join(TEMP_OUTPUT_DIR, f"sample_{i:04d}.pkl"), "wb") as f:
-        pickle.dump(d, f)
+# for i, d in enumerate(aligned_data_temp):
+#     with open(os.path.join(TEMP_OUTPUT_DIR, f"sample_{i:04d}.pkl"), "wb") as f:
+#         pickle.dump(d, f)
 
 # 輸出所有對齊資料為 pickle
 for i, d in enumerate(test_aligned_data):
